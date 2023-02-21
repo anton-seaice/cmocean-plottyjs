9940435f1627bc19b737f38a3388f443a88022b0
anton-climate <anton.steketee@anu.edu.au>
2023-02-21 15:59:54 +1100

    colorscales for plottyjs

--- make_pal.py
+++ make_pal.py
@@ -9,23 +9,28 @@ import os
 import cmocean
 
 
-# number of levels for colormap, set by ODV
-N = 113
+N = 256
 
 # location of local rgb files
 loc = 'https://raw.githubusercontent.com/matplotlib/cmocean/master/cmocean/rgb/'
 
-# file list
-Files = [loc + name + '-rgb.txt' for name in cmocean.cm.cmapnames]
-
 if not os.path.exists('pal'):
     os.makedirs('pal')
 
+# file name
+# fname = File.split('/')[-1].split('-')[0]
+f = open('colorscales.js', 'w')
+
+f.write('export const colorscales = { \n')
+
 # Loop through rgb files and make pal file
-for File in Files:
+for name in cmocean.cm.cmapnames:
+
+    # file import
+    file = loc + name + '-rgb.txt'
 
     # read in rgb values
-    rgb = np.genfromtxt(File)
+    rgb = np.genfromtxt(file)
 
     # convert to colormap
     cmap = cmocean.tools.cmap(rgb, N=N)
@@ -33,21 +38,20 @@ for File in Files:
     # back to rgb, now correct number of levels
     rgb = cmocean.tools.print_colormaps([cmap], N=N)[0]
 
-    # file name
-    fname = File.split('/')[-1].split('-')[0]
-    f = open('pal/' + fname + '.pal', 'w')
+    f.write(name+': new Uint8Array([')
 
-    # fill in lines 0-31 from ODV default palette
-    odv = np.loadtxt('pal/Odv-save.pal')
-    for j in range(32):
-        f.write('%3i  %.3f  %.3f  %.3f\n' % (odv[j, 0], odv[j, 1], odv[j, 2], odv[j, 3]))
+    for j in range(N):
+        f.write('%.0f, %.0f, %.0f, 255,' % (rgb[j, 0]*255, rgb[j, 1]*255, rgb[j, 2]*255))
 
-    # loop through rgb to write to file
-    for j in range(rgb.shape[0]):
-        f.write('%3i  %.3f  %.3f  %.3f\n' % (j+32, rgb[j, 0], rgb[j, 1], rgb[j, 2]))
+    f.write(']) ,\n')
 
-    # fill in lines 145-176
-    for j in range(145, 177):
-        f.write('%3i  %.3f  %.3f  %.3f\n' % (odv[j, 0], odv[j, 1], odv[j, 2], odv[j, 3]))
+    f.write(name+'_r : new Uint8Array([')
 
-    f.close()
+    for j in range(N):
+        f.write('%.0f, %.0f, %.0f, 255,' % (rgb[255-j, 0]*255, rgb[255-j, 1]*255, rgb[255-j, 2]*255))
+
+    f.write(']) ,\n')
+
+f.write('}')
+
+f.close()
